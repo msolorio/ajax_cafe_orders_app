8eb5930a7168e883a8a8752a2fd1c7b14fc923ce
msolorio <msolorio789@gmail.com>
2016-01-03 22:48:54 -0800

    ajax api code in dev

--- js/main.js
+++ js/main.js
@@ -1,4 +1,27 @@
 $(function(){
-	
+
+	var $currentOrders = $('.current-orders');
+	var $name = $('#name');
+	var $item = $('#item');
+
+	//GET REQUEST
+
+
+	$('#add-order').on('click', function(){
+		var order = {
+			name: $name.val(),
+			item: $item.val()
+		};
+
+		//POST REQUEST
+		$.ajax({
+			type: 'POST',
+			url: 'http://rest.learncode.academy/api/msolorio/orders',
+			data: order,
+			success: function(){
+				alert('the post request was successful');
+			}
+		});
+	});
 
 });
